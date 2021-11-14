2d82a81493acbeb630ff267f0479e01b6ec715a2
AcydAce <bayuhartho@gmail.com>
2021-11-14 21:00:45 +0700

    Update New Line Code.py

--- The Book Assignment/2 - New Line/New Line Code.py
+++ The Book Assignment/2 - New Line/New Line Code.py
@@ -4,11 +4,12 @@ Old_Text = open("Old_Text.txt", "r")
 oldContent = Old_Text.readlines()
 New_Text = open("New_Text.txt", "w")
 
-line = 1
+i = 1
 
-for i in Old_Text:
-    New_Text.write(f"{line} "+i)
+for line in Old_Text:
+    New_Text.write(f"{i} "+line)
     i += 1
+
 
 Old_Text.close()
 New_Text.close()
